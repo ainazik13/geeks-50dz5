8ba7be9439dc3ca1058228c4d8c3918f74c56d8c
user <mirlankyzyainazik13@gmail.com>
2025-05-29 12:35:13 +0600

    geeks-50dz5

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-function checkTrafficLight() {
-  const color = document.getElementById("colorInput").value.toLowerCase();
-  const message = document.getElementById("message");
+let color = prompt("Введите цвет светофора (красный, желтый, зеленый):").toLowerCase();
 
-  if (color === "красный") {
-    message.textContent = "Остановитесь!";
-  } else if (color === "желтый") {
-    message.textContent = "Приготовьтесь!";
-  } else if (color === "зеленый") {
-    message.textContent = "Можно ехать!";
-  } else {
-    message.textContent = "Ошибка: некорректный цвет светофора.";
-  }
+
+if (color === "красный") {
+  console.log("Остановитесь!");
+} else if (color === "желтый") {
+  console.log("Приготовьтесь!");
+} else if (color === "зеленый") {
+  console.log("Можно ехать!");
+} else {
+  console.log("Ошибка: некорректный цвет светофора.");
 }
